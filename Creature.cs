38762423697227ad5602037e30228ae2d6be88ae
Andreas Lorenz <andreas.lorenz1986@web.de>
2026-03-10 12:00:25 +0100

    Klasse Creature erstellt mit Konstruktor und Methode

---
+++ Creature.cs
@@ -0,0 +1,30 @@
+
+using System;
+
+namespace HogwartsSystem
+{
+    public class Creature
+    {
+        public string Name { get; set; }
+        public string Species { get; set; }
+        public int DangerLevel { get; set; }
+        public int Age { get; set; }
+
+        public Creature(string name, string species, int dangerLevel, int age)
+        {
+            Name = name;
+            Species = species;
+            DangerLevel = dangerLevel;
+            Age = age;
+        }
+
+        public void PrintInfo()
+        {
+            Console.WriteLine($"Name: {Name}");
+            Console.WriteLine($"Species: {Species}");
+            Console.WriteLine($"Danger Level: {DangerLevel}");
+            Console.WriteLine($"Age: {Age}");
+            Console.WriteLine("-------------------------");
+        }
+    }
+}
