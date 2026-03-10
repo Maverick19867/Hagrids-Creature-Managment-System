46dd7c9c97c741d22f2b75b159f0ae7e657eb8cd
Andreas Lorenz <andreas.lorenz1986@web.de>
2026-03-10 13:05:02 +0100

    Methode für AssignCreature(Student s, Creature c) erstellt

--- Assignment.cs
+++ Assignment.cs
@@ -1,1 +1,31 @@
 using System;
+using System.Diagnostics.Tracing;
+using System.Runtime.CompilerServices;
+using System.Security.Cryptography.X509Certificates;
+
+namespace HagridsCMS
+{
+    public class Assignment
+    {
+        public Student Student { get; set; }
+        public Creature Creature { get; set; }
+        public DateTime DateAssigned { get; set; }
+
+        public Assignment(Student student, Creature creature)
+        {
+            Student = student;
+            Creature = creature;
+            DateAssigned = DateTime.Now;
+        }
+
+        public void PrintAssignmentDetails()
+        {
+            Console.WriteLine("=== PFLEGE-ZUWEISUNG ===");
+            Console.WriteLine($"Schüler: {Student.Name} ({Student.House})");
+            Console.WriteLine($"Kreatur: {Creature.Name}der {Creature.Species}");
+            Console.WriteLine($"Gefahrenstufe der Aufgabe: {Creature.DangerLevel}");
+            Console.WriteLine($"Zugeordnet am: {DateAssigned.ToShortDateString()}");
+            Console.WriteLine("========================");
+        }
+    }
+}
--- Program.cs
+++ Program.cs
@@ -1,9 +1,12 @@
 ﻿using System;
 
+
 namespace HagridsCMS
 {
     class Program
     {
+
+        static List<Assigment> assigments = new List<Assigment>();
         static void Main(string[] args)
         {
             List<Creature> hagridsList = new List<Creature>();
@@ -27,7 +30,38 @@ namespace HagridsCMS
             students.Add(new Student("Cho Chang", "Ravenclaw", 4));
             students.Add(new Student("Seamus Finnigan", "Gryffindor", 3));
 
+            AssignCreature(students[0], hagridsList[7]);
+            AssignCreature(students[1], hagridsList[6]);
+            AssignCreature(students[2], hagridsList[5]);
+            AssignCreature(students[3], hagridsList[4]);
+            AssignCreature(students[4], hagridsList[3]);
+            AssignCreature(students[5], hagridsList[2]);
+            AssignCreature(students[6], hagridsList[1]);
+            AssignCreature(students[7], hagridsList[0]);
+
+            Console.WriteLine("===========================================");
+            Console.WriteLine("    HAGRIDS AKTUELLER PFLEGE-DIENSTPLAN    ");
+            Console.WriteLine("===========================================");
+            Console.WriteLine();
+
+            foreach (var job in assignments)
+            {
+                job.PrintAssignmentDetails();
+            }
+
+            Console.WriteLine("Drücke eine Taste zum Beenden...");
+            Console.ReadKey();
 
         }
+
+        public static void AssignCreature(Student s, Creature c)
+        {
+            MemberAssignment newAssignment = new Assignment(s,c);
+
+            assignments.Add(newAssignment);
+
+            Console.WriteLine($"System: {s.Name} wurde erfolgreich für {c.Name} eingetragen.");
+        }
+
     }
 }
